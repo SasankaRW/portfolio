95e233d1ff92d9fcc52a06ad7874e14f06df24b8
Sasanka Ravindu <61648299+SasankaRW@users.noreply.github.com>
2025-12-08 03:25:38 +0530

    Update socials.tsx

--- src/data/socials.tsx
+++ src/data/socials.tsx
@@ -27,16 +27,7 @@ export const socials: Social[] = [
             </svg>
         ),
     },
-    {
-        name: 'Email',
-        url: 'mailto:sasankarw@gmail.com',
-        icon: (
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-                <polyline points="22,6 12,13 2,6"></polyline>
-            </svg>
-        ),
-    },
+
     {
         name: 'Phone',
         url: 'tel:+94715669231',
